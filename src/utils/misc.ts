e74169ccf45238b3e0707bdd4c214707c7fce0d6
Hoziefa Alhassan <hoziefaalhasan@gmail.com>
2021-04-17 20:48:12 +0300

    Add space into card purchases count, Make Singleton pattern method a getter instead, cleanup manifest json.

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,3 @@
 export const formatNum = (num: number): string => num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 
-export const idGenerator = (prefix = 'item', length: number = Date.now()): string => `${ prefix }-${ Math.floor(Math.random() * length).toString(16) }`;
+export const idGenerator = (prefix = 'item', length = Date.now()): string => `${ prefix }-${ Math.floor(Math.random() * length).toString(16) }`;
